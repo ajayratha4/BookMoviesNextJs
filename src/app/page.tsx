9bf63837b23743387ca67f62e0e863c546fb80e4
ajayratha4 <ajayratha4@gmail.com>
2023-01-25 13:36:50 +0530

    Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
-const Page = () => {
-  return <div className="h-full flex-col flex ">Home</div>;
+import { use } from "react";
+async function getData() {
+  // You would usually fetch data from an API here.
+  // const res = await fetch("https://api.github.com/");
+
+  // But, here we just wait for 3 seconds.
+  await new Promise((res) => setTimeout(res, 3000));
+
+  // You would usually return data from an API here.
+  // return res.json();
+  return "Home  Components";
+}
+
+const Page = async () => {
+  // clintside
+  // const name = use(getData());
+
+  const name = await getData();
+
+  return <div className="h-full flex-col flex ">{name}</div>;
 };
 
 export default Page;
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,9 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   if (false) {
-    return NextResponse.redirect(request.nextUrl);
+    return NextResponse.redirect(new URL("/movies", request.url));
+  } else {
+    return NextResponse.next();
   }
 }
 
